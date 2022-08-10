11710780d8058fbf9c84106ac45089ce6b728760
ars1904 <serg190496@mail.ru>
2022-08-10 23:21:47 +0300

    Add

--- hh/hhapp/models.py
+++ hh/hhapp/models.py
@@ -4,6 +4,8 @@ from django.db.models import ManyToManyField, URLField, FloatField, IntegerField
 from usersapp.models import BlogUser
 # Create your models here.
 
+
+
 class Articles(Model):
     published = CharField(max_length=20)
     name = CharField(max_length=60)
@@ -11,9 +13,19 @@ class Articles(Model):
     image=models.ImageField(upload_to='posts', null=True, blank=True)
     user=models.ForeignKey(BlogUser, on_delete=models.CASCADE)
 
+    def has_image(self):
+        print('my image: ', self.image)
+        print('type', type(self.image))
+        return bool(self.image)
+    def some_method(self):
+        return 'hello from method'
+    def __str__(self):
+        return f'{self.name}'
     class Meta:
         verbose_name='Post'
         verbose_name_plural = 'Posts'
+
+
 
 class Tag(models.Model):
     name = models.CharField(max_length=16, unique=True)
