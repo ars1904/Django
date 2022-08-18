37d9f471461ae73a30094ec633d577d41d9f2182
ars1904 <serg190496@mail.ru>
2022-08-18 20:47:05 +0300

    Add

--- hh/hhapp/admin.py
+++ hh/hhapp/admin.py
@@ -2,6 +2,31 @@ from django.contrib import admin
 from .models import Articles, Tag, Category
 
 
-admin.site.register(Articles)
-admin.site.register(Tag)
 admin.site.register(Category)
+
+
+def clear_rating(modeladmin, request, queryset):
+    queryset.update(rating=1)
+
+
+clear_rating.short_description = "Выставить рейтинг = 1"
+
+
+def set_active(modeladmin, request, queryset):
+    queryset.update(is_active=True)
+
+
+class PostAdmin(admin.ModelAdmin):
+    list_display = ['name', 'category', 'has_image', 'rating', 'is_active']
+    actions = [clear_rating, set_active]
+
+
+admin.site.register(Articles, PostAdmin)
+
+
+class TagAdmin(admin.ModelAdmin):
+    #list_display = ['name', 'is_active']
+    actions = [set_active]
+
+
+admin.site.register(Tag, TagAdmin)
--- hh/hhapp/models.py
+++ hh/hhapp/models.py
@@ -6,12 +6,43 @@ from usersapp.models import BlogUser
 
 
 
-class Articles(Model):
+class Category(models.Model):
+    name = models.CharField(max_length=16, unique=True)
+    description = models.TextField(blank=True)
+
+
+
+class ActiveManager(models.Manager):
+    def get_queryset(self):
+        all_objects=super().get_queryset()
+        return all_objects.filter(is_active=True)
+
+class IsActiveMixin(models.Model):
+    objects = models.Manager()
+    active_objects = ActiveManager()
+    is_active = models.BooleanField(default=False)
+
+    class Meta:
+        abstract = True
+
+class Tag(IsActiveMixin):
+    name = models.CharField(max_length=16, unique=True)
+
+    def __str__(self):
+        return self.name
+
+class Articles(IsActiveMixin):
+    objects=models.Manager()
+    active_objects=ActiveManager()
     published = CharField(max_length=20)
     name = CharField(max_length=60)
     url = URLField()
     image=models.ImageField(upload_to='posts', null=True, blank=True)
     user=models.ForeignKey(BlogUser, on_delete=models.CASCADE)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    tags = models.ManyToManyField(Tag)
+    rating=models.PositiveSmallIntegerField(default=1)
+    is_active=models.BooleanField(default=False)
 
     def has_image(self):
         print('my image: ', self.image)
@@ -27,14 +58,7 @@ class Articles(Model):
 
 
 
-class Tag(models.Model):
-    name = models.CharField(max_length=16, unique=True)
 
-    def __str__(self):
-        return self.name
 
-class Category(models.Model):
-    name = models.CharField(max_length=16, unique=True)
-    description = models.TextField(blank=True)
 
 
